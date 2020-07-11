2f6ed4a48be011d2a8f113c211e7a1e6649f8b78
MoneyIgos <makowskiigor@gmail.com>
2020-07-11 15:33:45 +0200

    Add logo to app content

---
+++ src/components/Logo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="logo__wrapper">
+    <section class="logo">
+      <img src="./../assets/logo.png" />
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "logo"
+};
+</script>
+
+<style lang="scss">
+@use "./../styles/vars.scss";
+
+.logo {
+  margin: 50px 0;
+  display: flex;
+  justify-content: center;
+
+  img {
+    width: 80%;
+    height: 80%;
+  }
+}
+</style>
