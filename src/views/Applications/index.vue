e1227bb430d0ac02675e9b40561f2c9125e0a68e
MoneyIgos <makowskiigor@gmail.com>
2020-07-21 19:15:54 +0200

    Add applications template

--- src/views/Applications/index.vue
+++ src/views/Applications/index.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
-    <h1>This is an applications page</h1>
+    <div class="header__wrapper">
+      <header class="header">
+        <h1>Podania</h1>
+      </header>
+    </div>
+    <div class="content_wrapper">
+      <main>
+        <article class="content">
+          <div class="content__wl">Podanie na Whitelist</div>
+          <div class="content__sup">Podanie na Supporta</div>
+          <div class="content__lspd">Podanie na frakcję LSPD</div>
+          <div class="content__ems">Podanie na frakcję EMS</div>
+          <div class="content__lscm">Podanie na frakcję LSCM</div>
+          <div class="content__firm">Podanie na Firmę</div>
+          <div class="content__org">Podanie na Organizację Przestępczą</div>
+        </article>
+      </main>
+    </div>
   </div>
 </template>
