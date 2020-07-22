92c756527a9b3731db1fe4754d3c5ddcb3721efa
MoneyIgos <makowskiigor@gmail.com>
2020-07-22 12:34:03 +0200

    Style applications site

--- src/views/Applications/index.vue
+++ src/views/Applications/index.vue
@@ -8,15 +8,45 @@
     <div class="content_wrapper">
       <main>
         <article class="content">
-          <div class="content__wl">Podanie na Whitelist</div>
-          <div class="content__sup">Podanie na Supporta</div>
-          <div class="content__lspd">Podanie na frakcję LSPD</div>
-          <div class="content__ems">Podanie na frakcję EMS</div>
-          <div class="content__lscm">Podanie na frakcję LSCM</div>
-          <div class="content__firm">Podanie na Firmę</div>
-          <div class="content__org">Podanie na Organizację Przestępczą</div>
+          <div class="content__element"><p>Podanie na Whitelist</p></div>
+          <div class="content__element"><p>Podanie na Supporta</p></div>
+          <div class="content__element"><p>Podanie na frakcję LSPD</p></div>
+          <div class="content__element"><p>Podanie na frakcję LSCM</p></div>
+          <div class="content__element"><p>Podanie na frakcję LSCM</p></div>
+          <div class="content__element"><p>Podanie na Firmę</p></div>
+          <div class="content__element">
+            <p>Podanie na Organizację Przestępczą</p>
+          </div>
         </article>
       </main>
     </div>
   </div>
 </template>
+
+<style lang="scss">
+@use './../../styles/vars';
+
+.content__wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  &__element {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    border-radius: 25px;
+    background-color: vars.$dark-color;
+    width: 80vw;
+    margin: 10px;
+    //height: 10vh;
+  }
+}
+</style>
