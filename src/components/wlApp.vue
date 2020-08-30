21a1fd7fa27e048adf7f2a595252a954b2527da5
MoneyIgos <makowskiigor@gmail.com>
2020-08-30 12:08:17 +0200

    Add wlApp template

--- src/components/wlApp.vue
+++ src/components/wlApp.vue
@@ -6,6 +6,9 @@
     <main>
       <article class="content">
         <section class="content-ic">
+          <div class="content-ic__element--header">
+            <h3>Strefa IC</h3>
+          </div>
           <div class="content-ic__element">
             <form>
               <label class="content-ic__element-label"
@@ -55,6 +58,76 @@
                 class="content-ic__element-form"
                 name="ic/action"
                 label="Kreatywna akcja z udziałem twojej postaci.(nie wymagane)"
+              />
+            </form>
+          </div>
+        </section>
+        <section class="content-ooc">
+          <div class="content-ooc__element--header">
+            <h3>Strefa OOC</h3>
+          </div>
+          <div class="content-ic__element">
+            <form>
+              <label class="content-ic__element-label">
+                <b>OOC</b> | Wiek
+              </label>
+              <input
+                type="text"
+                class="content-ic__element-form"
+                name="ooc/old"
+                label="Twój wiek"
+              />
+            </form>
+          </div>
+          <div class="content-ic__element">
+            <form>
+              <label class="content-ic__element-label">
+                <b>OOC</b> | Co wiesz o RP?
+              </label>
+              <input
+                type="text"
+                class="content-ic__element-form"
+                name="ooc/rpKnow"
+                label="Co wiesz o RP"
+              />
+            </form>
+          </div>
+          <div class="content-ic__element">
+            <form>
+              <label class="content-ic__element-label">
+                <b>OOC</b> | Jakie masz doświadczenie w RP?
+              </label>
+              <input
+                type="text"
+                class="content-ic__element-form"
+                name="ooc/rpExperience"
+                label="Twoje doświadczenie w RP"
+              />
+            </form>
+          </div>
+          <div class="content-ic__element">
+            <form>
+              <label class="content-ic__element-label">
+                <b>OOC</b> | Nick Discord
+              </label>
+              <input
+                type="text"
+                class="content-ic__element-form"
+                name="ooc/dc"
+                label="Twój nick discord np.MoneyIgos#2000"
+              />
+            </form>
+          </div>
+          <div class="content-ic__element">
+            <form>
+              <label class="content-ic__element-label">
+                <b>OOC</b> | Steam HEX
+              </label>
+              <input
+                type="text"
+                class="content-ic__element-form"
+                name="ooc/old"
+                label="Twój Steam HEX np.11000011966ab8c"
               />
             </form>
           </div>
