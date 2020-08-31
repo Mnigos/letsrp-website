520847f9c01153b362f1cebdfb924de74cda6ea6
MoneyIgos <makowskiigor@gmail.com>
2020-08-31 18:51:28 +0200

    Add application send message

--- src/components/wlApp.vue
+++ src/components/wlApp.vue
@@ -14,7 +14,8 @@
               <div class="form__element-label"><b>IC</b> | Imię i Nazwisko</div>
               <input
                 type="text"
-                id="form__element-name"
+                id="name"
+                v-model="name"
                 class="form__element-input"
                 name="ic/name"
                 placeholder="Imię i Nazwisko twojej postaci."
@@ -28,6 +29,7 @@
                 type="text"
                 class="form__element-input"
                 id="form__element-date"
+                v-model="date"
                 name="ic/date"
                 placeholder="Data urodzenia twojej postaci.(dd-mm-rrrr)"
               />
@@ -42,8 +44,24 @@
                 type="text"
                 class="form__element-input"
                 name="ic/idea"
+                v-model="idea"
                 id="form__element-idea"
                 placeholder="Pomysł na twoją postać."
+              />
+            </form>
+          </div>
+          <div class="form__element">
+            <form>
+              <div class="form__element-label">
+                <b>IC</b> | Pomysł na Postać
+              </div>
+              <input
+                type="text"
+                class="form__element-input"
+                name="ic/story"
+                v-model="story"
+                id="form__element-story"
+                placeholder="Historia twojej postaci."
               />
             </form>
           </div>
@@ -54,6 +72,7 @@
                 type="text"
                 class="form__element-input"
                 name="ic/action"
+                v-model="action"
                 id="form__element-action"
                 placeholder="Kreatywna akcja z udziałem twojej postaci.(nie wymagane)"
               />
@@ -71,6 +90,7 @@
                 type="text"
                 class="form__element-input"
                 name="ooc/old"
+                v-model="old"
                 id="form__element-old"
                 placeholder="Twój wiek"
               />
@@ -83,6 +103,7 @@
                 type="text"
                 class="form__element-input"
                 name="ooc/rpKnow"
+                v-model="rpKnow"
                 id="form__element-rpKnow"
                 placeholder="Co wiesz o RP"
               />
@@ -96,7 +117,8 @@
               <input
                 type="text"
                 class="form__element-input"
-                name="ooc/rpExperience"
+                name="ooc/experience"
+                v-model="experience"
                 placeholder="Twoje doświadczenie w RP"
               />
             </form>
@@ -108,6 +130,7 @@
                 type="text"
                 class="form__element-input"
                 name="ooc/dc"
+                v-model="dc"
                 placeholder="Twój nick discord np.MoneyIgos#2000"
               />
             </form>
@@ -118,7 +141,8 @@
               <input
                 type="text"
                 class="form__element-input"
-                name="ooc/old"
+                name="ooc/hex"
+                v-model="hex"
                 placeholder="Twój Steam HEX np.11000011966ab8c"
               />
             </form>
@@ -126,7 +150,8 @@
         </section>
       </article>
       <div class="send-button-wrapper">
-        <button class="send-button">Wyślij Aplikację</button>
+        <div class="send-button-message" v-html="message"></div>
+        <button class="send-button" @click="submit()">Wyślij Aplikację</button>
       </div>
     </main>
   </div>
@@ -135,6 +160,36 @@
 <script>
 export default {
   name: 'wlApp',
+  data() {
+    return {
+      message: '',
+    };
+  },
+  methods: {
+    submit() {
+      if (!this.name || this.name.length < 8)
+        this.message =
+          'Imię i nazwisko twojej postaci musi zawierać co najmniej 8 znaków.';
+      if (!this.date)
+        this.message += '<br /> Musisz podać datę urodzenia twojej postaci.';
+      if (!this.idea)
+        this.message += '<br /> Musisz podać pomysł na twoją postać.';
+      if (!this.story || this.story.length < 200)
+        this.message +=
+          '<br /> Historia twojej postaci musi zawierać conajmniej 200 znaków.';
+      if (!this.old) this.message += '<br /> Musisz podać swój wiek.';
+      if (!this.rpKnow)
+        this.message +=
+          '<br /> Musisz podać swoją wiedzę o rozgrywkach Roleplay.';
+      if (!this.experience)
+        this.message +=
+          '<br /> Musisz podać swoje doświadczenie w rozgrywkach Roleplay.';
+      if (!this.dc || this.dc.length < 5)
+        this.message += '<br /> Niepoprawny nick discord.';
+      if (!this.hex || this.hex.length < 15)
+        this.message += '<br /> Niepoprawny steam HEX ID';
+    },
+  },
 };
 </script>
 
@@ -214,6 +269,12 @@ export default {
 
   &:hover {
     background-color: vars.$darker-color;
+  }
+
+  &-message {
+    color: #dd0000;
+    margin: 20px;
+    font-size: 0.8em;
   }
 }
 </style>
