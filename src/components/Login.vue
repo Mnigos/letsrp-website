d01b873e59d96b6bdd07954eb9e24c0c067f7472
MoneyIgos <makowskiigor@gmail.com>
2020-10-13 22:49:34 +0200

    Move login to another component

---
+++ src/components/Login.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="wrapper">
+    <div class="form">
+      <div class="form__login">
+        <form>
+          <input type="text" />
+        </form>
+      </div>
+      <div class="form__password">
+        <form>
+          <input :type="formPasswordType" />
+        </form>
+      </div>
+      <div class="form__checkbox">
+        <label>Pokaż hasło</label>
+        <input
+          type="checkbox"
+          @change="showPasswordAsText"
+          v-model="switchVisibility"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formPasswordType: 'password',
+      switchVisibility: false,
+    };
+  },
+  methods: {
+    showPasswordAsText() {
+      this.formPasswordType = this.switchVisibility ? 'password' : 'text';
+    },
+  },
+};
+</script>
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,40 +1,15 @@
 <template>
   <div class="wrapper">
-    <div class="form">
-      <div class="form__login">
-        <form>
-          <input type="text" />
-        </form>
-      </div>
-      <div class="form__password">
-        <form>
-          <input :type="formPasswordType" />
-        </form>
-      </div>
-      <div class="form__checkbox">
-        <label>Pokaż hasło</label>
-        <input
-          type="checkbox"
-          @change="showPasswordAsText"
-          v-model="switchVisibility"
-        />
-      </div>
-    </div>
+    <Login />
   </div>
 </template>
 
 <script>
+import Login from './components/Login';
+
 export default {
-  data() {
-    return {
-      formPasswordType: 'password',
-      switchVisibility: false,
-    };
-  },
-  methods: {
-    showPasswordAsText() {
-      this.formPasswordType = this.switchVisibility ? 'password' : 'text';
-    },
+  components: {
+    Login,
   },
 };
 </script>
