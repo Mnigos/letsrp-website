bc8c3955f0a8abceb3079758d035f57ac03e9b4d
MoneyIgos <makowskiigor@gmail.com>
2020-10-13 23:15:07 +0200

    Add style for Login component

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="wrapper">
     <div class="form">
-      <div class="form__login">
+      <div class="form__element">
         <form>
           <input type="text" />
         </form>
       </div>
-      <div class="form__password">
+      <div class="form__element">
         <form>
           <input :type="formPasswordType" />
         </form>
       </div>
-      <div class="form__checkbox">
+      <div class="form__element--checkbox">
         <label>Pokaż hasło</label>
         <input
           type="checkbox"
@@ -38,3 +38,47 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+@use '../styles/vars';
+
+.wrapper {
+  background-color: vars.$dark-color;
+  height: 100vh;
+  width: 100vw;
+  text-align: center;
+}
+
+.form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  &__element {
+    margin-top: 40px;
+
+    input {
+      border-radius: 10px;
+      height: 30px;
+      width: 300px;
+    }
+
+    &--checkbox {
+      display: flex;
+      justify-content: center;
+      margin-top: 40px;
+
+      input[type='checkbox'] {
+        margin-left: 30px;
+        border-radius: 10px;
+        height: 30px;
+        width: 30px;
+
+        &:checked {
+          background: vars.$green-color;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import Login from './components/Login';
+import Login from '../components/Login';
 
 export default {
   components: {
