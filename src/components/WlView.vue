19ea99501e12204344920c7582266d7d214dbfcd
MoneyIgos <makowskiigor@gmail.com>
2020-10-15 22:12:52 +0200

    Add navbar to admin panel

--- src/components/WlView.vue
+++ src/components/WlView.vue
@@ -4,6 +4,6 @@
 
 <script>
 export default {
-  name: 'AdminView',
+  name: 'WlView',
 };
 </script>
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,19 +1,85 @@
 <template>
   <div class="wrapper">
+    <div class="navbar__wrapper" ref="nav">
+      <nav class="navbar">
+        <div class="navbar__element">
+          <button @click="go('WlView')">Whitelist</button>
+        </div>
+        <div class="navbar__element">
+          <button @click="go('SupView')">Support</button>
+        </div>
+        <div class="navbar__element">
+          <button @click="go('LspdView')">LSPD</button>
+        </div>
+        <div class="navbar__element">
+          <button @click="go('EmsView')">EMS</button>
+        </div>
+        <div class="navbar__element">
+          <button @click="go('LscmView')">LSCM</button>
+        </div>
+        <div class="navbar__element">
+          <button @click="go('FirmView')">Firma</button>
+        </div>
+        <div class="navbar__element">
+          <button @click="go('OrgView')">Organizacja Przestępcza</button>
+        </div>
+      </nav>
+    </div>
     <!-- <Login /> -->
-    <AdminView />
+    <WlView />
   </div>
 </template>
 
 <script>
 import Login from '../components/Login';
-import AdminView from '../components/AdminView';
+import WlView from '../components/WlView';
+
+const pages = {
+  WlView,
+};
 
 export default {
   name: 'Admin',
   components: {
     Login,
-    AdminView,
+    WlView,
+  },
+  methods: {
+    async go(where) {
+      this.componentToShow = pages[where];
+      await this.$nextTick();
+      window.scrollTo({
+        top: this.$refs.component.$el.getBoundingClientRect().top,
+        behavior: 'smooth',
+      });
+    },
   },
 };
 </script>
+
+<style lang="scss" scoped>
+@use '../styles/vars';
+
+.wrapper {
+  background-color: vars.$dark-color;
+  height: 100vh;
+  width: 100vw;
+  text-align: center;
+}
+
+button {
+  color: #fff;
+  text-decoration: none;
+  outline: none;
+  border: none;
+  font-size: 24px;
+  font-family: sans-serif;
+  background-color: vars.$dark-color;
+}
+
+.navbar {
+  display: flex;
+  justify-content: center;
+  margin-right: 3vw;
+}
+</style>
