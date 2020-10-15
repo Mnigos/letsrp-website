30435facac2f3fe7f4ea9c87f154d98bc75811a7
MoneyIgos <makowskiigor@gmail.com>
2020-10-15 14:03:38 +0200

    Add AdminView component

---
+++ src/components/AdminView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="wrapper"></div>
+</template>
+
+<script>
+export default {
+  name: 'AdminView',
+};
+</script>
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="wrapper">
-    <Login />
+    <!-- <Login /> -->
+    <AdminView />
   </div>
 </template>
 
 <script>
 import Login from '../components/Login';
+import AdminView from '../components/AdminView';
 
 export default {
   name: 'Admin',
   components: {
     Login,
+    AdminView,
   },
 };
 </script>
